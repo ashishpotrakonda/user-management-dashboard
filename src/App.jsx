c94da613f84bab6ff5ab5a3257b2ab17c7c3af8e
Potrakonda Ashish <ashishpotrakonda@gmail.com>
2025-09-23 10:49:05 +0530

    Implement Add User Feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import { useState, useEffect } from "react";
 
 import Navbar from "./components/Navbar";
 import UserTable from "./components/UserTable";
+import UserFormModal from "./components/UserFormModal";
 
 import "./App.css";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [isUserFormModalOpen, setIsUserFormModalOpen] = useState(false);
+
+  const addUser = (user) => {
+    const id = users.length > 0 ? users[users.length - 1].id + 1 : 1;
+    setUsers([...users, { id, ...user }]);
+  };
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -25,7 +32,25 @@ function App() {
   return (
     <>
       <Navbar />
-      <UserTable users={users} />
+      <button
+        onClick={() => setIsUserFormModalOpen(true)}
+        className="px-4 py-2 mt-4 ml-4 bg-indigo-600 text-white text-sm rounded-lg shadow hover:bg-indigo-700 transition"
+      >
+        Add New User
+      </button>
+      <UserTable
+        users={users}
+        addUser={addUser}
+        onClose={() => setIsUserFormModalOpen(false)}
+      />
+      {isUserFormModalOpen && (
+        <UserFormModal
+          addUser={addUser}
+          onClose={() => {
+            setIsUserFormModalOpen(false);
+          }}
+        />
+      )}
     </>
   );
 }
---
+++ src/components/UserFormModal.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+
+const UserFormModal = ({ addUser, onClose }) => {
+  const [name, setName] = useState("");
+  const [company, setCompany] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name.trim() || !company.trim() || !email.trim()) return;
+
+    addUser({ name, company, email });
+    setName("");
+    setCompany("");
+    setEmail("");
+    onClose();
+  };
+
+  return (
+    <div className="fixed inset-0 bg-black/50 flex items-center justify-center">
+      <div className="bg-white p-7 rounded-lg w-[300px]">
+        <h2 className="text-xl font-semibold mb-2">Add User</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            className="w-full p-2 border border-gray-300 rounded mb-2"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            className="w-full p-2 border border-gray-300 rounded mb-2"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Company"
+            value={company}
+            className="w-full p-2 border border-gray-300 rounded mb-3"
+            onChange={(e) => setCompany(e.target.value)}
+          />
+          <div className="text-sm flex justify-end gap-2">
+            <button
+              type="button"
+              onClick={onClose}
+              className="px-3 py-1 rounded bg-gray-300 hover:bg-gray-400"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="px-3 py-1 rounded bg-green-500 text-white hover:bg-green-600"
+            >
+              Add
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default UserFormModal;
